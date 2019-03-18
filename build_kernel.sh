89ff492d6773eb92b4d2a0acf606bd92c3efe488
neo-hei <zhuorong_he@126.com>
2019-03-18 14:02:47 +0800

    修改了build_kernel.sh文件

--- build_kernel.sh
+++ build_kernel.sh
@@ -4,7 +4,8 @@ export ARCH=arm
 export CROSS_COMPILE=arm-none-linux-gnueabi-
 #make distclean
 #make imx_v7_defconfig
-make uImage LOADADDR=0x10800000 -j4
+make uImage LOADADDR=0x10800000 -j10
 make imx6q-iTop.dtb
-cp ./arch/arm/boot/uImage /media/sf_share/Mfgtools-Rel-1.1.0_121218_MX6Q_UPDATER/Profiles/MX6Q\ Linux\ Update/OS\ Firmware/files/linux/
-cp ./arch/arm/boot/dts/imx6q-iTop.dtb /media/sf_share/Mfgtools-Rel-1.1.0_121218_MX6Q_UPDATER/Profiles/MX6Q\ Linux\ Update/OS\ Firmware/files/linux/
+cp ./arch/arm/boot/uImage ~/share/tftp
+cp ./arch/arm/boot/dts/imx6q-iTop.dtb ~/share/tftp
+
